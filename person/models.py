d17dd29f184fe28b344de69c90c53cbc827bc239
jslee6091 <jslee6091@naver.com>
2021-01-15 18:02:42 +0900

    rest django

---
+++ person/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
